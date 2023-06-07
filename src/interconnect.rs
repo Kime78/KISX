164466b8713b09a7a81ded8e51e0dc6350cbc9d6
Kime <kime78.dev@gmail.com>
2023-06-07 19:43:31 +0300

    Interconect: implement reading from other areas than BIOS

--- src/interconnect.rs
+++ src/interconnect.rs
@@ -1,6 +1,5 @@
 use crate::bios::Bios;
 
-
 pub struct Interconnect {
     bios: Bios,
 }
@@ -11,11 +10,37 @@ impl Interconnect {
     }
 
     pub fn load32(&self, addr: u32) -> u32 {
+        if addr % 4 != 0 {
+            panic!("Unaligned load32 addr {:08x}", addr);
+        }
+
         if let Some(offset) = map::BIOS.contains(addr) {
-            return  self.bios.load32(offset);
+            return self.bios.load32(offset);
         }
 
         panic!("unhandled fetch32 at addr {:08x}", addr);
+    }
+
+    pub fn store32(&self, addr: u32, val: u32) {
+        if addr % 4 != 0 {
+            panic!("Unaligned store32 addr {:08x}", addr);
+        }
+
+        if let Some(offset) = map::MEMCONTROL.contains(addr) {
+            match offset {
+                0 => {
+                    if val != 0x1f000000 {
+                        panic!("Bad expansion 1 base addres 0x{:08x}", val);
+                    }
+                }
+                4 => {
+                    if val != 0x1f802000 {
+                        panic!("Bad expansion 2 base addres 0x{:08x}", val);
+                    }
+                }
+                _ => println!("Unhandled write to MEMCONSTROL register"),
+            }
+        }
     }
 }
 
@@ -28,12 +53,12 @@ mod map {
 
             if addr >= start && addr <= start + lenght {
                 Some(addr - start)
-            }
-            else {
+            } else {
                 None
             }
         }
     }
 
     pub const BIOS: Range = Range(0xbfc00000, 512 * 1024);
+    pub const MEMCONTROL: Range = Range(0x1f801000, 36);
 }
